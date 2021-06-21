d3c534f6e639f28c121b958b1fdcc7530b7e9762
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2021-06-21 20:14:16 +0800

    Removed imports from audio_graph.js
    Imports were not working. Removed them for now.

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-import "./audio_graph.js";
-import { drawFFT, drawWaveform } from "./drawing.js";
+import {
+    drawFFT,
+    drawWaveform
+} from "./drawing.js";
+
+var audioContext;
+var audioSourceNode;
+var sourceAnalyserNode;
+var outputAnalyserNode;
 
 const fft_samples = 2048;
-const frequency_bins = new Array(fft_samples/2);
-for (var i=0; i < frequency_bins.length; i++) {
-    frequency_bins[i] = (48000/2)/(frequency_bins.length) * i;
+const frequency_bins = new Array(fft_samples / 2);
+for (var i = 0; i < frequency_bins.length; i++) {
+    frequency_bins[i] = (48000 / 2) / (frequency_bins.length) * i;
 }
 // console.log(frequency_bins);
 
-
 const fft_canvas = document.getElementById("fft");
-const waveform_canvas = document.getElementById("waveform"); 
+const waveform_canvas = document.getElementById("waveform");
 
-window.onload = function() {
+window.onload = function () {
     console.log("window.onload");
     // for legacy browsers
     const AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -25,7 +31,10 @@ window.onload = function() {
 function getLocalStream() {
     document.body.removeEventListener('click', getLocalStream);
 
-    navigator.mediaDevices.getUserMedia({video: false, audio: true}).then( stream => {
+    navigator.mediaDevices.getUserMedia({
+        video: false,
+        audio: true
+    }).then(stream => {
         audioContext.resume().then(() => {
             console.log('Playback resumed successfully');
         });
@@ -39,7 +48,7 @@ function getLocalStream() {
         console.log(sourceAnalyserNode.maxDecibels);
         // Connect the audio source to the source analyer for analysis
         audioSourceNode.connect(sourceAnalyserNode);
-        
+
         var biquadFilter = new BiquadFilterNode(audioContext);
         // Connect up a biquadFilter
         sourceAnalyserNode.connect(biquadFilter);
@@ -54,32 +63,40 @@ function getLocalStream() {
         outputAnalyserNode.connect(audioContext.destination);
 
         window.requestAnimationFrame(draw);
-    }).catch( err => {
+    }).catch(err => {
         console.log("You got an error:" + err);
     });
 }
 
 function draw() {
-    for (var analyserNode_index=0; analyserNode_index<2; analyserNode_index++){
+    for (var analyserNode_index = 0; analyserNode_index < 2; analyserNode_index++) {
         var analyserNode;
         var strokeStyle;
-        switch(analyserNode_index) {
+        switch (analyserNode_index) {
             case 0:
                 analyserNode = sourceAnalyserNode;
-                strokeStyle =  'rgb(0, 0, 0)';
+                strokeStyle = 'rgb(0, 0, 0)';
                 break;
             case 1:
                 analyserNode = outputAnalyserNode;
-                strokeStyle =  'rgb(0, 255, 0)';
+                strokeStyle = 'rgb(0, 255, 0)';
                 break;
         }
-        
+
         if (analyserNode_index == 0) {
-            drawFFT(analyserNode, fft_canvas, true, {'strokeStyle': strokeStyle});
-            drawWaveform(analyserNode, waveform_canvas, true, {'strokeStyle': strokeStyle});
+            drawFFT(analyserNode, fft_canvas, true, {
+                'strokeStyle': strokeStyle
+            });
+            drawWaveform(analyserNode, waveform_canvas, true, {
+                'strokeStyle': strokeStyle
+            });
         } else {
-            drawFFT(analyserNode, fft_canvas, false, {'strokeStyle': strokeStyle});
-            drawWaveform(analyserNode, waveform_canvas, false, {'strokeStyle': strokeStyle});
+            drawFFT(analyserNode, fft_canvas, false, {
+                'strokeStyle': strokeStyle
+            });
+            drawWaveform(analyserNode, waveform_canvas, false, {
+                'strokeStyle': strokeStyle
+            });
         }
     }
     window.requestAnimationFrame(draw);
